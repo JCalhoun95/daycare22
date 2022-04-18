cf583217c69ca928303ab2a2dd7a5dd9a2eb94fe
Jamie Calhoun <JCalhoun_95@yahoo.com>
2022-04-18 18:35:30 -0500

    validation added

--- app/models/kids.rb
+++ app/models/kids.rb
@@ -1,6 +1,8 @@
 class Kid < ApplicationRecord
+  validates :name, presence:true, length: {minimum: 2, maximum: 100}
+  validates :description, presence:true, length: {minimum: 15, maximum: 300}
+  validates :age, presence:true, length: {minimum: 1, maximum: 3}
   belongs_to :families
   has_many :parents, through: :families
     scope :age_range, -> { where("RANGE(age) > ")}
-  end
 end
