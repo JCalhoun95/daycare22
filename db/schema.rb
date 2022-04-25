e62dc6206b380bf23edd89cc851e34f47dfd656e
Jamie Calhoun <JCalhoun_95@yahoo.com>
2022-04-24 19:16:03 -0500

    error fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_232337) do
+ActiveRecord::Schema.define(version: 2022_04_22_182234) do
 
   create_table "families", force: :cascade do |t|
     t.integer "kid_id"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2022_04_14_232337) do
 
   create_table "parents", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
