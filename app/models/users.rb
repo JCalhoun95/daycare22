985926c43b3dcc7b6b10d5493f0306e696daa594
Jamie Calhoun <JCalhoun_95@yahoo.com>
2022-04-24 19:50:59 -0500

    users controller added

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
